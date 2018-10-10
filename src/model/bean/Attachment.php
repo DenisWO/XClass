e63393fdb431fadb940462e134020970e18e5ff2
Gabriel Mesquita <gabriel_t_ana@hotmail.com>
2018-10-10 17:55:19 -0300

    Arrumando as classes

---
+++ src/model/bean/Attachment.php
@@ -0,0 +1,91 @@
+<?php
+
+  class Attachment{
+
+    var $id;
+    var $directory;
+    var $filename;
+    var $extension;
+    var $created_at; // Timestamp object
+    var $updated_at; // Timestamp object
+
+    public function __construct($id , $directory , $filename , $extension , $created_at , $updated_at) {
+      $this->setId($id);
+      $this->setDirectory($directory);
+      $this->setFilename($filename);
+      $this->setExtension($extension);
+      $this->setCreated_at($created_at);
+      $this->setUpdated_at($updated_at);
+    }
+
+    public function __construct( $directory , $filename , $extension ) {
+      $this->setDirectory($directory);
+      $this->setFilename($filename);
+      $this->setExtension($extension);
+      $this->setCreated_at();
+      $this->setUpdated_at();
+    }
+
+    public function getId() {
+      return $this->id;
+    }
+
+    public function setId($id) {
+      $this->id = $id;
+    }
+
+    public function getDirectory() {
+      return $this->directory;
+    }
+
+    public function setDirectory($directory) {
+      $this->directory = $directory;
+    }
+
+    public function getFilename() {
+      return $this->filename;
+    }
+
+    public function setFilename($filename) {
+      $this->filename = $filename;
+    }
+
+    public function getExtension() {
+      return $this->extension;
+    }
+
+    public function setExtension($extension) {
+      $this->extension = $extension;
+    }
+
+    public function getCreated_at() {
+      return $this->created_at;
+    }
+
+    private function setCreated_at() {
+      if (empty($this->created_at)) {
+        $this->created_at = date('Y-m-d H:i:s');
+      }else{
+        throw new Created_atException("Can only be created once");
+      }
+    }
+
+    private function setCreated_at($date) {
+      $this->created_at = $date
+    }
+
+    public function getUpdated_at() {
+      return $this->updated_at;
+    }
+
+    private function setUpdated_at() {
+      $this->updated_at = date('Y-m-d H:i:s');
+    }
+
+    private function setUpdated_at($date) {
+      $this->updated_at = $date;
+    }
+
+  }
+
+?>
---
+++ src/model/bean/User.php
@@ -0,0 +1,151 @@
+<?php
+
+  class User{
+
+    private $id;
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $password;   // Nao sei se a senha deve ficar aqui !!!!
+    private $age;
+    private $created_at; // Object Timestamp
+    private $updated_at; // Object Timestamp
+    private $photo;      // Object Attachment -> Profile Photo
+    private $thumbnail;  // Object Attachment -> Low resolution profile photo
+
+    public function __construct($id , $firstName , $lastName , $email , $password , $age , $created_at , $updated_at , $photo , $thumbnail) {
+      $this->setId($id);
+      $this->setFirstName($firstName);
+      $this->setLastName($lastName);
+      $this->setEmail($email);
+      $this->setPassword($password);
+      $this->setAge($age);
+      $this->setCreated_at();
+      $this->setUpdated_at();
+      $this->setPhoto($photo);
+      $this->thumbnail($thumbnail);
+
+      // Verificar se a photo e thumbnail estao no BDA (funcao clase AttachmentManager)
+    }
+
+    public function __construct($firstName , $lastName , $email , $password , $age) {
+      $this->setFirstName($firstName);
+      $this->setLastName($lastName);
+      $this->setEmail($email);
+      $this->setPassword($password);
+      $this->setAge($age);
+      $this->setCreated_at();
+      $this->setUpdated_at();
+
+      // add photo default using AttachmentManager
+      // add thumbnail default using AttachmentManager
+    }
+    public function __construct($name, $email, $password){
+      $this->setFirstName($name);
+      $this->setLastName("");
+      $this->setEmail($email);
+      $this->setPassword($password);
+      $this->setCreated_at();
+      $this->setUpdated_at();
+      $this->setAge(0);
+    }
+
+    public function getId() {
+      return $this->id;
+    }
+
+    public function setId($id) {
+      $this->id = $id;
+    }
+
+    public function getFirstName() {
+      return $this->firstName;
+    }
+
+    public function setFirstName($firstName) {
+      $this->firstName = $firstName;
+    }
+
+    public function getLastName() {
+      return $this->lastName;
+    }
+
+    public function setLastName($lastName) {
+      $this->lastName = $lastName;
+    }
+
+    public function getEmail() {
+      return $this->email;
+    }
+
+    public function setEmail($email) {
+      $this->email-> $email;
+    }
+
+    public function getPassword() {
+      return $this->password;
+    }
+
+    public function setPassword($password) {
+      $this->password = $password;
+    }
+
+    public function getAge() {
+      return $this->age;
+    }
+
+    public function setAge($age) {
+      if ($age < 0 | $age >120) {
+        throw new WrongAgeException("Idade fora do padrão","Improbable age" , $age);
+      }else{
+        $this->age = $age;
+      }
+    }
+
+    public function getCreated_At() {
+      return $this->created_at;
+    }
+
+    private function setCreated_at() {
+      if (empty($this->created_at)) {
+        $this->created_at = date('Y-m-d H:i:s');
+      }else{
+        throw new Created_atException("Can only be created once");
+      }
+    }
+
+    private function setCreated_at($date) {
+      $this->created_at = $date
+    }
+
+    public function getUpdated_At() {
+      return $this->updated_at;
+    }
+
+    private function setUpdated_at() {
+      $this->updated_at = date('Y-m-d H:i:s');
+    }
+
+    private function setUpdated_at($date) {
+      $this->updated_at = $date;
+    }
+
+    public function getPhoto() {
+      return $this->photo;
+    }
+
+    private function setPhoto($photo) {
+      $this->photo = $photo;
+    }
+
+    public function getThumbnail() {
+      return $this->thumbnail;
+    }
+
+    private function setThumbnail($thumbnail) {
+      $this->thumbnail = $thumbnail;
+    }
+
+  }
+
+?>
