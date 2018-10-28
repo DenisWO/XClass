04574c2b4299df478d8eea21963d69c1ce367ff2
DenisWO <o.denis96@gmail.com>
2018-10-28 15:11:44 -0300

    Inserindo dados na classe Class

--- src/model/bean/Class.php
+++ src/model/bean/Class.php
@@ -2,10 +2,75 @@
 
   class Class{
 
-    public function __construct() {
-      // Construction default
+    private $id;
+    private $name;
+    private $instituiton;
+    private $teacherId;
+    private $year;
+    private $semester;    //Porque precisa de um semestre para a turma?
+    private $created_at; //Object  Timestamp
+    private $updated_at; //Object Timestamp
+
+
+    public function __construct($id, $name, $instituiton, $teacherId, $year, $semester, $updated_at, $created_at) {
+      $this->setId($id);
+      $this->setName($name);
+      $this->setInstituiton($instituiton);
+      $this->setTeacherId($teacherId);
+      $this->setYear($year);
+      $this->setSemester($semester);
+      $this->setUpdated_at($updated_at);
+      $this->setCreated_at($created_at);
+    }
+    public function getId(){
+      return $this->id;
+    }
+    public function getName(){
+      return $this->name;
+    }
+    public function getInstituiton(){
+      return $this->instituiton;
+    }
+    public function getTeacherId(){
+      return $this->teacherId;
+    }
+    public function getYear(){
+      return $this->year;
+    }
+    public function getSemester(){
+      return $this->semester;
+    }
+    public function getCreated_At(){
+      return $this->created_at;
+    }
+    public function getUpdated_At(){
+      return $this->updated_at;
+    }
+    public function setId($id){
+      $this->id = $id;
+    }
+    public function setName($name){
+      $this->name = $name;
+    }
+    public function setInstituiton($instituiton){
+      $this->instituiton = $instituiton;
+    }
+    public function setTeacherId($teacherId){
+      $this->teacherId = $teacherId;
+    }
+    public function setYear($year){
+      $this->year = $year;
+    }
+    public function setSemester($semester){
+      $this->semester = $semester;
+    }
+    public function setUpdated_at(){
+      $this->updated_at = $updated_at;
+    }
+    public function setCreated_at(){
+      $this->created_at = $created_at;
     }
 
   }
-  
+
 ?>
--- src/model/dao/UserDAO.php
+++ src/model/dao/UserDAO.php
@@ -16,14 +16,10 @@
       if (get_class($objectUser) == "User") {
         throw new WrongObjectException("Wrong object" , "User" , get_class($objectUser));
       }
-
       $sql = "SELECT * FROM user WHERE email = '$objectUser->email'";
       if($conector->query($sql) === NULL){
         $sql = 'INSERT INTO User (first_name , last_name , email, password , birthday , created_at , updated_at , photo_id , thumbnail_id) VALUES (?,?,?,?,?,?,?,?,?)';
         $stmt = $conector->prepare($sql);
-
-
-
         $stmt->bind_param("sssssssii", $objectUser->getFirstName(), $objectUser->getLastName() , $objectUser->getEmail(), $objectUser->getPassword() , $objectUser->getBirthday() , $objectUser->getCreated_at() , $objectUser->getUpdated_at() , $objectUser->getPhoto()->getId() , $objectUser->getThumbnail()->getId() );
 
         if (!$stmt) {
@@ -49,7 +45,6 @@
 
       //Refresh updated_at
       $objectUser->setUpdated_at();
-
       $sql = "UPDATE User SET first_name = ? , last_name = ? , email = ? , password = ? , birthday = ? , created_at = ? , updated_at = ? , photo_id = ? , thumbnail_id = ? WHERE id = ? "
       $stmt = $conector->prepare($sql);
       $stmt->bind_param("sssssssiii",  $objectUser->getFirstName() , $objectUser->getLastName() , $objectUser->getEmail() , $objectUser->getPassword() , $objectUser->getBirthday() , $objectUser->getCreated_at() , $objectUser->getUpdated_at() , $objectUser->getPhoto()->getId() , $objectUser->getThumbnail()->getId() , $objectUser->getId() );
@@ -68,12 +63,45 @@
 
     //Load ALL users
     public function loadAll() {
-
+      
     }
 
     //Loads only the id specific user
     public function loadId($id) {
+      $sql = "SELECT * FROM user WHERE email = {'$id'}";
+      $conect = $conector->query($sql);
 
+      if($dados = $conector->query($sql) === TRUE){
+
+        try {
+          //Capture the profile photo in the bda
+          $photoDAO = new AttachmentDAO();
+          $photo = $photoDAO->loadId($data["photo_id"]);
+
+          //Capture the thumbnail in the bda
+          $thumbnailDAO = new AttachmentDAO();
+          $thumbnail = $thumbnailDAO->loadId($data["thumbnail_id"]);
+
+          $user = new User($data["id"],
+            $data["firstName"],
+            $data["lastName"],
+            $data["email"],
+            $data["password"],
+            $data["birthday"],
+            $data["created_at"],
+            $data["updated_at"],
+            $photo,
+            $thumbnail
+          );
+          return $user;
+        }catch(SQLException $e) {
+          throw $e;
+        }
+      }else{
+        throw new SQLException("Não foi possivel executar a query" , $stmt , $sql);
+      }
+
+      throw new UnregistredUserException("Id incorreto" , $id);
     }
 
     //Loads only the email specific user
