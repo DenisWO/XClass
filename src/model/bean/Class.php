2cfeb826ec4dcc1974567e37b2e49a63dfe8de5c
DenisWO <o.denis96@gmail.com>
2018-10-30 12:51:02 -0300

    Alterando dados da classe Class
    - Retirando atributos inúteis da classe
    - Incluindo objetos do tipo User
    - Alterando métodos de acordo com os atributos atuais

--- src/model/bean/Class.php
+++ src/model/bean/Class.php
@@ -5,22 +5,20 @@
     private $id;
     private $name;
     private $instituiton;
-    private $teacherId;
-    private $year;
-    private $semester;    //Porque precisa de um semestre para a turma?
+    private $teacher;  //Objeto usuario, declarado como professor
     private $created_at; //Object  Timestamp
     private $updated_at; //Object Timestamp
+    private $students; //Array de objetos usuarios, usados como alunos
 
 
-    public function __construct($id, $name, $instituiton, $teacherId, $year, $semester, $updated_at, $created_at) {
+    public function __construct($id, $name, $instituiton, $teacher, $updated_at, $created_at) {
       $this->setId($id);
       $this->setName($name);
       $this->setInstituiton($instituiton);
-      $this->setTeacherId($teacherId);
-      $this->setYear($year);
-      $this->setSemester($semester);
+      $this->setTeacher($teacher);
       $this->setUpdated_at($updated_at);
       $this->setCreated_at($created_at);
+      $this->students = array();
     }
     public function getId(){
       return $this->id;
@@ -31,20 +29,17 @@
     public function getInstituiton(){
       return $this->instituiton;
     }
-    public function getTeacherId(){
-      return $this->teacherId;
-    }
-    public function getYear(){
-      return $this->year;
-    }
-    public function getSemester(){
-      return $this->semester;
+    public function getTeacher(){
+      return $this->teacher;
     }
     public function getCreated_At(){
       return $this->created_at;
     }
     public function getUpdated_At(){
       return $this->updated_at;
+    }
+    public function getStudents(){
+      return $this->students;
     }
     public function setId($id){
       $this->id = $id;
@@ -55,20 +50,27 @@
     public function setInstituiton($instituiton){
       $this->instituiton = $instituiton;
     }
-    public function setTeacherId($teacherId){
-      $this->teacherId = $teacherId;
-    }
-    public function setYear($year){
-      $this->year = $year;
-    }
-    public function setSemester($semester){
-      $this->semester = $semester;
+    public function setTeacher($teacher){
+      if(isset($teacher)){
+        $this->teacher = $teacher;
+      }
     }
     public function setUpdated_at(){
       $this->updated_at = $updated_at;
     }
     public function setCreated_at(){
       $this->created_at = $created_at;
+    }
+    public function setStudents($student){
+      if(isset($student)){
+        array_push($this->students, $student);
+      }
+    }
+    public function removeStudent($student){
+      $index = array_search($student, $this->students)
+      if($index){
+        unset($this->students[$index]);
+      }
     }
 
   }
