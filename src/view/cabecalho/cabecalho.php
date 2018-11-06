674ba4418c84fb37589d389f48715104767081f9
danieldhsd <danieldhsd@yahoo.com.br>
2018-11-06 17:27:58 -0200

    Pequena alteracao

---
+++ src/view/cabecalho/cabecalho.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">
+    <img src="../../resources/image/xclass.png" alt="Logo" style="width:40px;">
+    XClass
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Turmas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Calendário</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Criar Turma</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Participar de Turma</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Meu Perfil</a>
+      </li>
+    </ul>
+    <form class="form-inline" action="/action_page.php">
+        <input class="form-control mr-sm-2" type="text" placeholder="Procurar">
+        <button class="btn btn-success" type="submit">Procurar</button>
+    </form>
+  </div>  
+</nav>
+
+</body>
+</html>
