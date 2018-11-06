d9db760daaffe92a895a752f30a4e2d3571cc3f1
danieldhsd <danieldhsd@yahoo.com.br>
2018-11-06 17:31:00 -0200

    Main Page

--- src/view/pagprincipal/pagprincipal.php
+++ src/view/pagprincipal/pagprincipal.php
@@ -10,38 +10,10 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">
-    <img src="../../resources/image/xclass.png" alt="Logo" style="width:40px;">
-    XClass
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Turmas</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Calendário</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Criar Turma</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Participar de Turma</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Meu Perfil</a>
-      </li>
-    </ul>
-    <form class="form-inline" action="/action_page.php">
-        <input class="form-control mr-sm-2" type="text" placeholder="Procurar">
-        <button class="btn btn-success" type="submit">Procurar</button>
-    </form>
-  </div>  
-</nav>
+    <?php
+        include_once "../cabecalho/cabecalho.php";
+    ?>
 
+    
 </body>
 </html>
