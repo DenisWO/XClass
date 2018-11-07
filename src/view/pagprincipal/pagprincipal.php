62f4a058c965146a7c511a14b80122ef46a49088
danieldhsd <danieldhsd@yahoo.com.br>
2018-11-07 18:10:50 -0200

    Pagina das turmas

--- src/view/pagprincipal/pagprincipal.php
+++ src/view/pagprincipal/pagprincipal.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Classes</title>
+    <link rel="stylesheet" href="../../resources/css/pagprincipal.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
@@ -14,6 +16,20 @@
         include_once "../cabecalho/cabecalho.php";
     ?>
 
-    
+    <div class="container">
+        <div id="turma">
+            <div id="disProf">
+                <div class="text-dark" id="nomeDisciplina">
+                    <h3>Nome Disciplina</h3>
+                </div>
+                <div class="text-dark text-small" id="nomeProf">
+                    <h5>Nome Professor</h5>
+                </div>
+            </div>
+            <div id="imagemPerfil">
+                <img src="../../resources/image/xclass.png" width="80" heidth="80">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
