fdb676380f8245b448392337299743d886b4910c
Gabriel Mesquita <gabriel_t_ana@hotmail.com>
2018-11-14 18:18:22 -0200

    Estilizado pagina de edição de perfil Roubado style da tela de cadastro do Daniel hehe

--- src/view/profile/editProfile.php
+++ src/view/profile/editProfile.php
@@ -12,46 +12,135 @@
 
 ?>
 
-<!doctype "html">
-<html lang = "pt-br">
+<!doctype html>
+<html lang="en">
   <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../resources/image/xclass.png">
+
+    <title>Cadastrar</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../resources/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../../resources/css/bootstrap/form-validation.css" rel="stylesheet">
   </head>
-  <body>
-    <div>
-      <form action="" method="post" name="cadastro" >
-        <h1>Meu perfil</h1>
 
-        <div>
-          <br><label>Primeiro Nome:</label>
-          <input type="text" name="first_name" value="<?php echo $user->getFirstName() ?>"/>
-          <br><label>Ultimo Nome:</label>
-          <input type="text" name="last_name" value="<?php echo $user->getLastName() ?>"/>
-          <br><label>Email:</label>
-          <input type="text" name="email" value="<?php echo $user->getEmail() ?>"/>
-          <br><label>Senha:</label>
-          <input type="text" name="password" value="<?php echo $user->getPassword() ?>"/>
-          <br><label>Data nascimento:</label>
-          <input type="text" name="birthday" value="<?php echo $user->getBirthday() ?>"/>
+  <body class="bg-light">
+
+    <div class="container">
+      <div class="py-5 text-center">
+        <img class="d-block mx-auto mb-4" src="../../resources/image/xclass.png" alt="" width="300" height="150">
+        <h2>Perfil do Usuário</h2>
+        <p class="lead">Modifique seus dados XClass!</p>
+      </div>
+
+      <div class="row">
+        <div class="col-md-8 order-md-1">
+          <h4 class="mb-3">Dados Cadastrais</h4>
+          <form class="needs-validation" method="POST" action="">
+            <div class="row">
+              <div class="col-md-6 mb-3">
+                <label for="firstName">Nome</label>
+                <input type="text" class="form-control" name="firstName" id="firstName" placeholder="" value="<?php echo $user->getFirstName() ?>" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="lastName">Sobrenome</label>
+                <input type="text" class="form-control" name="lastName" id="lastName" placeholder="" value="<?php echo $user->getLastName() ?>" required>
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label for="email">E-mail <span class="text-muted"></span></label>
+              <input type="email" class="form-control" name="email" id="email" placeholder="ex@gmail.com" value="<?php echo $user->getEmail() ?>">
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label for="password">Senha</label>
+              <input type="password" class="form-control" name="password" id="password" placeholder="Senha" value="<?php echo $user->getPassword() ?>" required>
+              <div class="invalid-feedback">
+                Digite sua senha.
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-5 mb-3">
+                <label for="birthday">Data de Nascimento</label>
+                    <div class="mb-3">
+                        <input type="date" name="birthday" id="birthday" class="form-control" value="<?php echo $user->getBirthday() ?>">
+                    </div>
+              </div>
+            </div>
+
+            <input class="btn btn-primary btn-lg btn-block" type="submit" value="Salvar modificações">
+          </form>
+
+          <form>
+            <div class="py-5 text-center">
+              <h2>Modificar foto de perfil</h2>
+            </div>
+            <div>
+              <p class="lead">Imagem de Perfil:</p>
+              <img src="<?php $photo = $user->getPhoto(); echo $backToRoot . $photo->getAddress(); ?>"/>
+            </div>
+
+            <div>
+              <p class="lead">Thumbnail:</p>
+              <img src="<?php $thumbnail = $user->getThumbnail(); echo $backToRoot . $thumbnail->getAddress(); ?>"/>
+            </div>
+
+            <br><input class="btn btn-primary btn-lg btn-block" type="submit" value="Alterar Foto">
+          </form>
+
         </div>
+      </div>
 
-        <div>
-          <br><label>Imagem de Perfil:</label><br><br>
-          <img src="<?php $photo = $user->getPhoto(); echo $backToRoot . $photo->getAddress(); ?>"/>
-        </div>
-
-        <div>
-          <br><label>Thumbnail:</label><br><br>
-          <img src="<?php $thumbnail = $user->getThumbnail(); echo $backToRoot . $thumbnail->getAddress(); ?>"/>
-        </div>
-
-        <div>
-          <br><input type="submit" name="salvar" value="Salvar" />
-        </div>
-
-        <div><br><br><a href="../../index.php">Voltar Menu</a></div>
-      </form>
+      <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2017-2018 XClass</p>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Privacidade</a></li>
+          <li class="list-inline-item"><a href="#">Termos</a></li>
+          <li class="list-inline-item"><a href="#">Suporte</a></li>
+        </ul>
+      </footer>
     </div>
 
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <script>
+      // Example starter JavaScript for disabling form submissions if there are invalid fields
+      (function() {
+        'use strict';
+
+        window.addEventListener('load', function() {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      })();
+    </script>
   </body>
 </html>
