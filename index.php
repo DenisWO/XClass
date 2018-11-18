cb4db952c0e8b836ab0f1a25d3edfbf70dd6da2f
Guilherme Fernandes <guifernandes1371@gmail.com>
2018-11-18 20:41:33 -0200

    Atualizando Index

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
-<?php
-  //Prea esta encarregado de criar o menu deste arquivo
-  header('Location:./src/view/login/index.php');
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="src/resources/image/xclass.png">
 
-?>
+    <title>Bem vindo o XClass</title>
+
+    <link href="src/resources/css/pagprincipal.css" rel="stylesheet">
+
+  </head>
+
+  <body id="corpoIndex">
+  	
+  	<div>
+  		
+  		<h1 id="titulo"> Bem vindo ao Portal Acadêmico - XClass</h1>
+
+  	</div>
+
+  	<div id="links">
+
+  			<a href="src/view/login/index.php"> <img src="src/resources/image/login_image.jpg"> </a>
+
+	  		<a href="src/view/cadastro/index.html"> <img src="src/resources/image/cad_image.jpg"> </a>
+
+	  	<div>	
+	  		<img src="src/resources/image/brasao_uit.jpeg">
+	  	</div>
+
+	  	</div>
+
+	  		<p id="Sobre"> Sistema de gerenciamento de arquivos acadêmicos, que tem como objetivo organizar todo o tipo de conteúdo acadêmico ministrado em um determinado curso profissionalizante, fundamental, médio, graduação, pós-graduação, etc. Acesse já e aproveite! </p>
+
+	  	<div>
+  	
+  	</div>
+
+  </body>
+
+</html>
+
